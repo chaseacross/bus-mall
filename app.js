a1eff580ea5b12cb3db6b395f93d674a4da680c6
Chase Cross <chaseacross@gmail.com>
2017-02-26 22:07:09 -0800

    Got the three images to be generated randomly upon page load

--- app.js
+++ app.js
@@ -6,11 +6,21 @@
 
 //Only using the 'bs' to start just ot make debugging easier.
 var productImageNames = ['bag.jpg', 'banana.jpg', 'bathroom.jpg', 'boots.jpg', 'breakfast.jpg', 'bubblegum.jpg',];
+var productSelections =[]
 
-//'Not sure why that last element in the array is discolored? -- Inquire later//
 
 //I know I'm going to need to count the total number of clicks regardless of how I generate images//
 var clickCounter = 0;
+
+//Using an IFFE to generate random pictures on page load//
+(function loadingImages(){
+  var num1 = Math.floor(Math.random() * (productImageNames.length));
+  var num2 = Math.floor(Math.random() * (productImageNames.length));
+  var num3 = Math.floor(Math.random() * (productImageNames.length));
+  document.canvas1.src = productImageNames[num1];
+  document.canvas2.src = productImageNames[num2];
+  document.canvas3.src = productImageNames[num3];
+})();
 
 //Need to grab a random number to later pick three random pictures from the array//
 function displayImage(){
@@ -23,15 +33,15 @@ function displayImage(){
     document.canvas3.src = productImageNames[num3];
     clickCounter++;
   } //If clickCounter < 15
-};
+};//display image button//
 
+
+//using this to test counter function//
 function counterButton(){
-  alert('Click counter is at ' + clickCounter +'.')
+  alert('Click counter is at ' + clickCounter +'.');
+  alert(productSelections);
 }
 
-
-
-//  for (var i = 0; i < 3; i++) // FOR LOOP
 
 
 // // a simple IIFE to build all the product images
